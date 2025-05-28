d08c02e3574626aee98285a3123ea2c8f08e92df
Roman Fernandez Salch <fernandezsalchr@gmail.com>
2025-05-28 00:43:36 -0300

    Diseño Responsive

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import './style.css'
+
+const btnMenu = document.getElementById('btnMenu')
+const header = document.getElementById('header')
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+
+/* console.log(open);
+console.log(close);
+console.log(btnMenu); */
+btnMenu.addEventListener('click', () => {
+    header.classList.toggle('-translate-x-full')
+    btnMenu.classList.toggle('bg-texto')
+    btnMenu.classList.toggle('bg-hover')
+    open.classList.toggle('hidden')
+    close.classList.toggle('hidden')
+})
